4714606e3f21ce56369545f7af41e504b971bd5e
ISSOtm <eldredhabert0@gmail.com>
2021-08-24 20:54:16 +0200

    Get rid of nested macro
    Allows compiling with RGBDS 0.4.2 and later

--- sample_macros.asm
+++ sample_macros.asm
@@ -5,20 +5,6 @@ include_sample: MACRO
 SAMPLE_BANK = \4
 CUR_POS = 0
 LABEL_DEFINED = 0
-
-; sample_slice label, path, size
-sample_slice: MACRO
-    SECTION "\1 sample bank {SAMPLE_BANK}", ROMX[$8000 - (\3)],BANK[SAMPLE_BANK]
-    IF !LABEL_DEFINED
-LABEL_DEFINED = 1
-\1
-    ENDC
-    INCBIN \2, CUR_POS, \3
-
-CUR_POS = CUR_POS + (\3)
-SAMPLE_BANK = SAMPLE_BANK + 1
-ENDM
-
     ; Ensure sample ends on a bankswitch
     IF CUR_POS % $4000
         sample_slice \1, \2, CUR_POS % $4000
@@ -28,13 +14,27 @@ ENDM
         sample_slice \1, \2, $4000
     ENDR
 
-End\1 ; Define end of sample label
+End\1: ; Define end of sample label
 
     IF LOW(SAMPLE_BANK) == $FF
         WARN "Smooth-Player may glitch out if a sample ends on bank $FF or $1FF"
     ELIF HIGH(\4) != HIGH(SAMPLE_BANK)
         WARN "Smooth-Player does not natively support playback of samples across banks 127 and 128"
     ENDC
+ENDM
+
+; sample_slice label, path, size
+; DO NOT CALL YOURSELF!
+sample_slice: MACRO
+    SECTION "\1 sample bank {SAMPLE_BANK}", ROMX[$8000 - (\3)],BANK[SAMPLE_BANK]
+    IF !LABEL_DEFINED
+LABEL_DEFINED = 1
+\1:
+    ENDC
+    INCBIN \2, CUR_POS, \3
+
+CUR_POS = CUR_POS + (\3)
+SAMPLE_BANK = SAMPLE_BANK + 1
 ENDM
 
 
